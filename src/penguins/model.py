f188967db5edfe0e4ca30384ad6c9363dc2a21b5
Sega NDIAYE <ndiaye.sega.pro@gmail.com>
2025-08-28 12:41:56 +0200

    Add docker fastapi

--- src/penguins/model.py
+++ src/penguins/model.py
@@ -10,12 +10,11 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_sc
 
 
 from penguins.registry import save_model, load_model
-from penguins.params import MLFLOW_EXPERIMENT_NAME, MLFLOW_TRACKING_URI
+from penguins.params import MLFLOW_EXPERIMENT_NAME, MLFLOW_URI
 
 logger = loguru.logger
 
-mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
+
 
 def instantiate_model(fit=True):
     if fit:
@@ -29,6 +28,8 @@ def train_model(model, X_train:pd.DataFrame, y_train:pd.Series):
     return model
 
 def evaluate_model(model, X:pd.DataFrame, y:pd.Series) -> tuple[float]:
+    mlflow.set_tracking_uri(MLFLOW_URI)
+    mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
     y_pred = model.predict(X)
     score =  round(accuracy_score(y, y_pred), 2)
     recall = round(recall_score(y, y_pred, average="macro"), 2)
